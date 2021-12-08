8db04ff4e7abd16e95bb420b83a0c4590d634465
Chase-Stearns <90790258+Chase-Stearns@users.noreply.github.com>
2021-12-08 05:54:20 -0600

    Homework 10

---
+++ HW10.py
@@ -0,0 +1,39 @@
+"""
+For this homework you will not need to write any code. Instead you will write a
+short paragraph answering the below question. You will save this paragraph in a
+textfile called HW10 and upload it to your github repo.
+
+
+Question 01)
+
+Briefly describe a computing project you would find interesting to work on in
+the future. This project may be
+
+1) Implementing existing mathematics as algorithms, as we have done in this
+course.
+
+2) A simple project to learn another computing language.
+
+3) An addition to the linear algebra package from your final project. 
+
+4) A project to learn more complex tools in python. (Such as numpy, scipy,
+pandas, machine learning packages such as tensorflow, etc...)
+
+5) A computer application (such as a video game, art tools, music editor, etc..)
+
+6) Any other type of project which I have failed to mention here. 
+
+
+Your answer does not need to be exceptionally detailed. Just a short paragraph.
+"""
+
+#Homework 10
+"""
+A computing project I would find interesting to work on is trying to create a 
+computer game that is simple. I think a computer would be the easiest and the
+most enjoyable to create because it would not be as difficult and time 
+consuming. Video games are my biggest hobby and I believe a project like this 
+would help me undertsand more about how complex the games we all know and love
+truly are.
+
+"""
